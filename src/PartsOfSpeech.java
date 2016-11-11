484911402c3cfdbe3b0a9777e62bc991bf395b83
Daniel Hair <danielmhair@gmail.com>
2016-11-11 09:04:25 -0700

    merged

--- src/PartsOfSpeech.java
+++ src/PartsOfSpeech.java
@@ -1,6 +1,21 @@
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class PartsOfSpeech {
+    private Map<String, ArrayList<Pair<String, Double>>> transitionModel;
 
+    PartsOfSpeech() {
+        this.transitionModel = new HashMap<>();
+    }
 
+    private void modelLanguage(String[] words) {
+        for (String word: words) {
+            if (this.transitionModel.getOrDefault(word, ))
+        }
+    }
 
     public static void main(String[] args) {
 
