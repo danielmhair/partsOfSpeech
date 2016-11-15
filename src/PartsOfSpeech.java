7723899380c825b4c616480b6cbe17815a987164
Samuel G-C <Samuel G-C>
2016-11-15 00:45:43 -0700

    text generation works

--- src/PartsOfSpeech.java
+++ src/PartsOfSpeech.java
@@ -4,12 +4,15 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class PartsOfSpeech {
     private Map<String, ArrayList<ProbabilityPair>> transitionModel;
+    private Random rand;
 
     PartsOfSpeech() {
         this.transitionModel = new HashMap<>();
+        rand = new Random();
     }
 
     public class ProbabilityPair {
@@ -44,6 +47,14 @@ public class PartsOfSpeech {
 }
     }
 
+    private ProbabilityPair find(ArrayList<ProbabilityPair> list, String key) {
+        for (ProbabilityPair pair : list) {
+            if(pair.getKey().equals(key))
+                return pair;
+        }
+        return null;
+    }
+
     private void modelLanguage(ArrayList<String> words, int n) {
         ArrayList<String[]> ngrams = new ArrayList<>();
         for (int i = 0; i < words.size(); i++) {
@@ -63,22 +74,56 @@ public class PartsOfSpeech {
             ArrayList<ProbabilityPair> list;
             if (this.transitionModel.get(ngram[0]) != null)
                 list = this.transitionModel.get(ngram[0]);
-            else
+            else {
                 list = new ArrayList<>();
-            if (!list.contains(ngram[ngram.length-1])) {
+                this.transitionModel.put(ngram[0], list);
+            }
+            if (find(list, ngram[ngram.length-1]) == null) {
                 list.add(new ProbabilityPair(ngram[ngram.length-1], 1.0));
             }
             else {
-                ProbabilityPair pair = list.get(list.indexOf(ngram[ngram.length-1]));
+                ProbabilityPair pair = list.get(list.indexOf(find(list, ngram[ngram.length-1])));
                 pair.setValue(pair.getValue() + 1.0);
             }
         }
-
-        System.out.println(this.transitionModel.toString());
     }
 
     private void updateProbabilities() {
+        for (Map.Entry<String, ArrayList<ProbabilityPair>> entry : this.transitionModel.entrySet())
+        {
+            Double total = 0.0;
+            for (ProbabilityPair pair : entry.getValue())
+                total += pair.getValue();
+            for (ProbabilityPair pair : entry.getValue())
+                pair.setValue(pair.getValue()/total);
+        }
+    }
 
+    private String generateWord(ArrayList<ProbabilityPair> list, Double value) {
+        String result = "";
+        HashMap<ProbabilityPair, Double> probably = new HashMap<>();
+        Double max = 0.0;
+        for (ProbabilityPair pair : list) {
+            max += pair.getValue();
+            probably.put(pair, max);
+        }
+        for (Map.Entry<ProbabilityPair, Double> entry : probably.entrySet()) {
+            if (value < entry.getValue() && value >= entry.getValue() - entry.getKey().getValue())
+                result = entry.getKey().getKey();
+        }
+        return result;
+    }
+
+    private String generateText(String seed, int count) {
+        String result = seed;
+        for (int i = 0; i < count; i++) {
+            result += " ";
+            ArrayList<ProbabilityPair> list = this.transitionModel.get(seed);
+            Double value = rand.nextDouble();
+            seed = generateWord(list, value);
+            result += seed;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
@@ -95,6 +140,8 @@ public class PartsOfSpeech {
             POS.modelLanguage(words, 2);
             POS.updateProbabilities();
 
+            String genesis = POS.generateText(args[1], 25);
+            System.out.println(genesis);
         } catch (IOException e) {
             e.printStackTrace();
         }
